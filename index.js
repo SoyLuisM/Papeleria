d18c2f44404eecab870c4fba2b660276566061ee
Luis Martinez <s_k_ap3@hotmail.com>
2021-05-31 20:33:35 -0500

    agregando funciones para evitar fallas

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const os = require('os');
 
 const app = express()
 const { config } = require("./config")
@@ -13,3 +14,28 @@ router(app)
 app.listen(config.port,() => {
       console.log(`Escuchando en http://localhost:${config.port}`)
 })
+
+//capura promesas no capturadas
+process.on('unhandledRejection',error =>{
+      // console.error('UnhandledRejection',error)
+      console.error('unhandledRejection',error)
+})
+//captura excepciones no capturadas
+process.on('uncaughtException',error =>{
+      // console.error('UncaughtException',error)
+      console.error('UncaughtException',error)
+})
+
+setInterval(alertMem,3000)
+
+function alertMem(){
+      free = os.freemem()
+      total = os.totalmem()
+      porcentaje = ((free/1024)*100)/(total/1024)
+      if(porcentaje < 25){
+            console.info(`memoria por debajo del ${porcentaje}%`)
+      }
+      else if(porcentaje<20){
+            console.warn(`memoria por debajo del ${porcentaje}`)
+      }
+}
